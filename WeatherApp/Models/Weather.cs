cc43674085cc7efab8334afc0a2e84b867fb9665
mhtechguy <mhniceguy@gmail.com>
2018-08-18 21:50:39 -0400

    Version 1.0 of app

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string appid = "82fe51335ada2a172bf15266e5ffba46";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=" + appid + "&units=imperial";
+            //synchronous client.
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+
+    }
+}
